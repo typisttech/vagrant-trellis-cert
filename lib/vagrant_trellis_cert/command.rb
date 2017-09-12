dc231c230a7d3984cda597e09abbe2ec0ab08b70
Tang Rufus <tangrufus@gmail.com>
2017-09-12 09:31:40 +0800

    bundle exec rubocop --auto-correct

--- lib/vagrant_trellis_cert/command.rb
+++ lib/vagrant_trellis_cert/command.rb
@@ -12,18 +12,18 @@ module VagrantTrellisCert
     def execute
       options = {}
       opts = OptionParser.new do |o|
-        o.banner = 'Usage: vagrant trellis-cert [--path <path>]'
-        o.separator ''
+        o.banner = "Usage: vagrant trellis-cert [--path <path>]"
+        o.separator ""
         o.version = VagrantSudoRsync::Identity.version
-        o.program_name = 'vagrant trellis-cert'
+        o.program_name = "vagrant trellis-cert"
 
-        o.on('--path <path>', String, 'Path to the Trellis root') do |path|
-            options[:path] = path
+        o.on("--path <path>", String, "Path to the Trellis root") do |path|
+          options[:path] = path
         end
       end
       argv = parse_options(opts)
 
-      @path = options[:path] || '.'
+      @path = options[:path] || "."
 
       FileUtils.rm_rf(tmp_path)
       FileUtils.mkdir_p(tmp_path)
@@ -47,16 +47,16 @@ module VagrantTrellisCert
 
     def canonical_hosts
       site_hosts.map do |host|
-        if !host.is_a?(Hash) || !host.has_key?('canonical')
-          fail_with_message File.read(File.join(@path, 'roles/common/templates/site_hosts.j2')).sub!('{{ env }}', 'development').gsub!(/com$/, 'dev')
+        if !host.is_a?(Hash) || !host.has_key?("canonical")
+          fail_with_message File.read(File.join(@path, "roles/common/templates/site_hosts.j2")).sub!("{{ env }}", "development").gsub!(/com$/, "dev")
         end
 
-        host.fetch('canonical')
+        host.fetch("canonical")
       end
     end
 
     def site_hosts
-      sites.flat_map { |(_name, site)| site['site_hosts'] }
+      sites.flat_map { |(_name, site)| site["site_hosts"] }
     end
 
     def sites
@@ -64,13 +64,13 @@ module VagrantTrellisCert
         fail_with_message "#{config_file} was not found. Please run `$ vagrant trellis-cert` with `--path` option"
       end
 
-      YAML.load_file(config_file)['wordpress_sites'].tap do |sites|
+      YAML.load_file(config_file)["wordpress_sites"].tap do |sites|
         fail_with_message "No sites found in #{config_file}." if sites.to_h.empty?
       end
     end
 
     def config_file
-      File.join(@path, 'group_vars', 'development', 'wordpress_sites.yml')
+      File.join(@path, "group_vars", "development", "wordpress_sites.yml")
     end
 
     def fail_with_message(msg)
